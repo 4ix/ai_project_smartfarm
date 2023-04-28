549531a21d9148fbc42111c1275dac657e868de7
4ix <jinjihoon@gmail.com>
2023-04-28 11:02:54 +0900

    cctv 화면 내 화면 재생

--- lib/screens/cctv1_screen.dart
+++ lib/screens/cctv1_screen.dart
@@ -1,17 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 import '../models/colors_model.dart';
 
-class CctvOne extends StatelessWidget {
+class CctvOne extends StatefulWidget {
   const CctvOne({super.key});
+
+  @override
+  State<CctvOne> createState() => _CctvOneState();
+}
+
+class _CctvOneState extends State<CctvOne> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+        'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4')
+      ..initialize().then((_) {
+        setState(() {});
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(color: ColorsModel.second),
-      child: const Column(
+      child: Column(
         children: [
-          Padding(
+          const Padding(
             padding: EdgeInsets.all(10),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -27,14 +45,34 @@ class CctvOne extends StatelessWidget {
               ],
             ),
           ),
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Image(
-              image: AssetImage('assets/cctv.png'),
-            ),
+          // Image(
+          //   image: AssetImage('assets/cctv.png'),
+          // ),
+          Center(
+            child: _controller.value.isInitialized
+                ? AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: GestureDetector(
+                      onTap: () {
+                        if (_controller.value.isPlaying) {
+                          _controller.pause();
+                        } else {
+                          _controller.play();
+                        }
+                      },
+                      child: VideoPlayer(_controller),
+                    ),
+                  )
+                : Container(),
           ),
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
   }
 }
